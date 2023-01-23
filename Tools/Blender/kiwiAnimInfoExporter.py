90384676866ee17a40fec5f032bf5a62a9281c4a
Josh B <josh@epichousestudios.com>
2023-01-22 18:23:43 -0800

    [#115] Finish animation info exporter

--- Tools/Blender/kiwiAnimInfoExporter.py
+++ Tools/Blender/kiwiAnimInfoExporter.py
@@ -9,6 +9,14 @@ unregister_class = kiwiCommon.unregister_class
 
 # ==================================================================================
 
+g_exportPathIsValid = False
+
+def OnSet_export_path(self, value):
+	g_exportPathIsValid
+	return None
+
+# ==================================================================================
+
 class PropertyGroup_KiwiTools_Object(bpy.types.PropertyGroup):
 	object: bpy.props.PointerProperty(
 		type=bpy.types.ID,
@@ -31,13 +39,16 @@ class SceneProperties_KiwiTools_AnimInfoExport(bpy.types.PropertyGroup):
 		description="If enabled, exports animation",
 		default=True,
 		)
+		
+	# TODO: note down list of Markers that are to be used as events
 	
 	export_path: bpy.props.StringProperty(
 		name="Export Directory",
 		description="Path to directory where the files are created",
 		default="//",
 		maxlen=1024,
-		subtype="DIR_PATH",
+		subtype="FILE_PATH",
+		#set=OnSet_export_path,
 		)
 
 # ==================================================================================
@@ -96,8 +107,6 @@ class View3DPanel_KiwiTools_AnimInfoExporter(bpy.types.Panel):
 		layout.prop(context.scene.kiwitools_animinfo, "export_path", text="")
 		
 		# Need a button for "export"
-		
-		# Looping thru animation, also check Markers
 		layout.operator("kiwitools.animinfo_export", text="Export", icon='EXPORT')
 		
 
@@ -133,15 +142,133 @@ class Operator_KiwiTools_AnimInfoExport(bpy.types.Operator):
 	bl_label = "Animation Info Export"
 	bl_description = "Export marker data and selected objects' animation transformation."
 
+	@classmethod
+	def poll(cls, context):
+		import os
+		try:
+			# Check if the export path is a vaid filename
+			path_blender = bpy.path.abspath(context.scene.kiwitools_animinfo.export_path)
+			path_check = os.path.realpath(path_blender, strict=False)
+			base_name = os.path.basename(path_check)
+
+			if (path_check is not None) and (base_name is not None) and (not os.path.isdir(path_check)):
+				return True
+			else:
+				return False
+		except:
+			return False
+
 	def execute(self, context):
 		#from . import export
 		#ret = export.write_mesh(context, self.report)
-		ret = 0
+		# Call into the exporter now
+		ret = ExportAnimationInfo(
+			scene=context.scene,
+			context=context,
+			export_filepath=context.scene.kiwitools_animinfo.export_path,
+			markers_enabled=context.scene.kiwitools_animinfo.export_markers
+			)
 
 		if ret:
 			return {'FINISHED'}
 
 		return {'CANCELLED'}
+		
+# ==================================================================================
+
+def ExportAnimationInfo(export_filepath, scene: bpy.types.Scene=None, context: bpy.types.Context=None, markers_enabled=False):
+	import os
+	import math
+
+	# Generate the filename first
+	export_absfilepath = bpy.path.abspath(export_filepath)
+	export_basename = str(os.path.basename(export_absfilepath)).rsplit('.', 1)[0]
+	export_filename = os.path.realpath(os.path.dirname(export_absfilepath) + "/" + export_basename + ".kai")
+	export_path = os.path.dirname(export_filename)
+
+	# first ensure the path is created
+	if export_path:
+		# this can fail with strange errors,
+		# if the dir can't be made then we get an error later.
+		try:
+			os.makedirs(export_path, exist_ok=True)
+		except:
+			import traceback
+			traceback.print_exc()
+
+	# Open the output file
+	fout = open(export_filename, mode="wt")
+	if fout is None:
+		return False
+
+	# Export the animation info
+	fout.write(f"begin {scene.frame_start}\n")
+	fout.write(f"end {scene.frame_end}\n")
+
+	# If markers are enabled, then roll through the animation and output begin & end based on markers.
+	if markers_enabled:
+		fout.write("subanims\n")
+		fout.write("{\n")
+
+		# Loop through all markers to get the subanims. There's specific naming requirements.
+		bInSubAnimation = False
+		subAnimationName = ""
+		subAnimationStart = 0
+
+		marker: bpy.types.TimelineMarker
+		for marker in sorted(scene.timeline_markers, key=lambda marker: marker.frame):
+			# If any marker is noted as an "event" ignore it.
+			if "event" in marker.name:
+				continue
+			
+			# Otherwise, our marker marks an animation
+			if not bInSubAnimation:
+				# Save animation start
+				subAnimationName = marker.name
+				subAnimationStart = marker.frame
+				bInSubAnimation = True
+			elif "end" in marker.name or subAnimationName in marker.name:
+				bInSubAnimation = False
+				# If we found the end of the anim, we write it out
+				fout.write(f"\t{subAnimationName} {subAnimationStart} {marker.frame}\n")
+		fout.write("}\n")
+
+	# Export the transform info
+	frame_old = scene.frame_current
+	
+	object_ref: PropertyGroup_KiwiTools_Object
+	for object_ref in scene.kiwitools_animinfo.tracked_transforms:
+		# For each one, roll through frame-by-frame and export TRS information
+		tracked_object = object_ref.object
+		tracked_object_converted = bpy.props.PointerProperty(type=bpy.types.Object)
+		tracked_object_converted = tracked_object
+
+		fout.write(f"attachment {tracked_object_converted.name}\n")
+		fout.write("{\n")
+		for frame in range(scene.frame_start, scene.frame_end):
+			# Set frame & prep to evaluate the frame
+			scene.frame_set(frame)
+			depsgraph = context.evaluated_depsgraph_get()
+			evaluated_object = tracked_object_converted.evaluated_get(depsgraph)
+
+			translation	= evaluated_object.matrix_world.to_translation()
+			rotation	= evaluated_object.matrix_world.to_euler('XYZ')
+			scale		= evaluated_object.matrix_world.to_scale()
+
+			fout.write(f"\t{frame}")
+			fout.write(f" {translation.x:0.12f} {translation.y:0.12f} {translation.z:0.12f}")
+			fout.write(f" {math.degrees(rotation.x):0.12f} {math.degrees(rotation.y):0.12f} {math.degrees(rotation.z):0.12f}")
+			fout.write(f" {scale.x:0.12f} {scale.y:0.12f} {scale.z:0.12f}\n")
+
+		fout.write("}\n")
+
+	scene.frame_set(frame_old)
+
+	# Close file now that we're done
+	fout.close()
+
+	# We did it!
+	return True
 
 # ==================================================================================
 
